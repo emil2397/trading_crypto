11c519f61d08e0d5bffeadfe8a95bd832ad1a74f
alexpros <alexander.a.proskuryakov@gmail.com>
2022-08-29 23:18:22 +0300

    Minor fix.

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ warnings.filterwarnings("ignore")
 
 GOOGLE_KEY_PATH = "./local_data/google_key.json"
 SAVED_STATE_PATH = "./local_data/dashboard.json"
+ALERT_FORMAT = ["alert", "ticker", "timeframe", "pattern", "time"]
 
 dashboard = Dashboard(google_key_path=GOOGLE_KEY_PATH, saved_state_path=SAVED_STATE_PATH)
 app = Flask(__name__)
@@ -23,8 +24,8 @@ def hello():
     alert_time = datetime.datetime.now()
 
     data = request.get_data(cache=True, as_text=True, parse_form_data=True)
-    request_data = data.split(",")[1,2,4]
-    request_data = [request_data]
+    request_data = dict(zip(format, data.split(",")))
+    request_data = [[request_data["ticker"], request_data["timeframe"], request_data["patter"]]]
 
     print("New alert: ", request_data)
 
